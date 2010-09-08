0bf07e25c22180ea4e8b9b6a28ea892c25b4e105
David Raynes <rayners@gmail.com>
2010-09-08 18:50:25 -0400

    Added the new .el files to init

--- init.el
+++ init.el
@@ -4,7 +4,13 @@
 
 (load "rayners/private")
 
+; basic stuff
+(load "rayners/paths")
+(load "rayners/buffers")
+
 ; load the gui bits
 (load "rayners/gui")
+
+;; Mode setups
 (load "rayners/erc")
 (load "rayners/org")
