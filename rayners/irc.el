2bb63bcd3b5ce80180f6249e643d88b6ed11f406
David Raynes <rayners@gmail.com>
2010-09-17 10:54:38 -0400

    IRC fanciness, extra rcirc configy bits

--- rayners/irc.el
+++ rayners/irc.el
@@ -7,6 +7,35 @@
 ; plus I might not be back on the VPN
 (setq erc-server-auto-reconnect nil)
 
+(setq rayners/which-irc 'rcirc)
+(setq rayners/freenode-channels 
+      '("#git" "#emacs" "#perl" "#movabletype" "#movabletype-talk"))
+(cond ((eq rayners/which-irc 'rcirc)
+       (add-to-list 'rayners/freenode-channels "#rcirc"))
+      ((eq rayners/which-irc 'erc)
+       (add-to-list 'rayners/freenode-channels "#erc")))
+
+(load "rayners/private/irc.el")
+
+; channels should be all setup now
+; erc needs the channels set, by server, it erc-autojoin-channels-alist
+(if (eq rayners/which-irc 'erc)
+    (add-to-list 
+     'erc-autojoin-channels-alist 
+     (cons "freenode.net" rayners/freenode-channels)))
+
+(defun rayners/freenode-connect ()
+  "Connect to freenode.net"
+  (interactive)
+  (cond ((eq rayners/which-irc 'rcirc)
+	 (rcirc-connect "irc.freenode.net" "6667" 
+			"rayners" "rayners" "David Raynes" 
+			rayners/freenode-channels))
+	((eq rayners/which-irc 'erc)
+	 (erc :server "irc.freenode.net" :port 6667 :nick "rayners" :full-name "David Raynes"
+))))
+(global-set-key (kbd "C-c I f") 'rayners/freenode-connect)
+
 (defun djcb-erc-start-or-switch ()
   "Connect to ERC, or switch to last active buffer"
   (interactive)
@@ -46,3 +75,36 @@
 (setq rcirc-default-nick "rayners")
 (setq rcirc-default-user-name "rayners")
 (setq rcirc-default-full-name "David Raynes")
+
+(add-hook 'rcirc-mode-hook
+	  (lambda ()
+	    (rcirc-track-minor-mode 1)))
+
+(setq rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY"))
+
+(add-hook 'rcirc-mode-hook (lambda ()
+			     (flyspell-mode 1)))
+
+(eval-after-load 'rcirc
+  '(defun-rcirc-command reconnect (arg)
+     "Reconnect the server process."
+     (interactive "i")
+     (unless process
+       (error "There's no process for this target"))
+     (let* ((server (car (process-contact process)))
+	    (port (process-contact process :service))
+	    (nick (rcirc-nick process))
+	    channels query-buffers)
+       (dolist (buf (buffer-list))
+	 (with-current-buffer buf
+	   (when (eq process (rcirc-buffer-process))
+	     (remove-hook 'change-major-mode-hook
+			  'rcirc-change-major-mode-hook)
+	     (if (rcirc-channel-p rcirc-target)
+		 (setq channels (cons rcirc-target channels))
+	       (setq query-buffers (cons buf query-buffers))))))
+       (delete-process process)
+       (rcirc-connect server port nick
+		      rcirc-default-user-name
+		      rcirc-default-user-full-name
+		      channels))))
