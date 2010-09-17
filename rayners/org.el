5f1ed5b9749e33abd3504507f4e9179ad7e41bea
David Raynes <rayners@gmail.com>
2010-09-17 15:45:40 -0400

    Build the list of org files for the agenda

--- rayners/org.el
+++ rayners/org.el
@@ -3,3 +3,14 @@
 
 ; round clock times to 15m increments
 (setq org-time-stamp-rounding-minutes (quote (1 15)))
+
+; need to set the list of files
+; first, shared files
+(setq rayners/shared-org-files 
+      (file-expand-wildcards "~/Dropbox/projects/*.org"))
+; now the personal ones
+(setq rayners/private-org-files 
+      (file-expand-wildcards "~/Dropbox/org/*.org"))
+
+(setq org-agenda-files 
+      (append rayners/shared-org-files rayners/private-org-files))
