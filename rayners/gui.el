b96373a24a3d0397c84fece2fc774315f6b294be
David Raynes <rayners@gmail.com>
2010-11-15 20:38:56 -0500

    Widen emacs windows (frames) by default, thanks to new code standards being used (120 line length)

--- rayners/gui.el
+++ rayners/gui.el
@@ -18,3 +18,5 @@
 ;; because this barfs otherwise
 (if (not noninteractive)
     (set-face-font 'default "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1"))
+
+(add-to-list 'default-frame-alist '(width . 120))
