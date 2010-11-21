0476bd43ed3292d56964ffcd4cbf831037b72002
David Raynes <rayners@gmail.com>
2010-11-21 01:50:15 -0500

    And an updated rayners/perl.el to go with them

--- rayners/perl.el
+++ rayners/perl.el
@@ -7,3 +7,13 @@
   '(add-hook 'cperl-mode-hook 'perlcritic-mode))
 
 ;; need to add some perltidy bits in here too
+(autoload 'perltidy      "perltidy-mode" nil t)
+(autoload 'perltidy-mode "perltidy-mode" nil t)
+
+(eval-after-load "cperl-mode"
+  '(add-hook 'cperl-mode-hook 'perltidy-mode))
+
+(global-set-key "\C-ct" 'perltidy)
+
+; normal perltidy settings
+(setq perltidy-bin "perltidy -q -pbp")
