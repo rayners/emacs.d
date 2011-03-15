5c7e45eb6b6d00d6c218ea25325e74e32f33f7a6
David Raynes <rayners@gmail.com>
2011-03-14 20:58:01 -0400

    Stupid smartypants in code

--- gnus.el
+++ gnus.el
@@ -1,6 +1,6 @@
 (setq gnus-nntp-server nil)
 (setq gnus-select-method 
-      '(nnimap "" 
-		(nnimap-address “imap.gmail.com”)
+      '(nnimap "gmail" 
+		(nnimap-address "imap.gmail.com")
 		(nnimap-server-port 993)
 		(nnimap-stream ssl)))
