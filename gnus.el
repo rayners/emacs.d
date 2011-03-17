7ad966d1fc5d2a329e60dc10f8cf7f8d9ff32a60
David Raynes <rayners@gmail.com>
2011-03-17 01:19:02 -0400

    More poking around for getting mail working in gnus

--- gnus.el
+++ gnus.el
@@ -4,3 +4,9 @@
 		(nnimap-address "imap.gmail.com")
 		(nnimap-server-port 993)
 		(nnimap-stream ssl)))
+
+(setq gnus-parameters
+      '((".*" ; this may not need to be this drastic
+	 (display . all))))
+(setq gnus-permanently-visible-groups ".*")
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
